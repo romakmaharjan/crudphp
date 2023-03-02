4d8da9598933bb181abd918681ddc49d227f76c8
romak maharjan <romakmaharjan@gmail.com>
2023-03-02 13:06:43 +0545

    Added update and delete table on display php file

--- display.php
+++ display.php
@@ -47,27 +47,15 @@ include 'connect.php';
                     <td>'.$email.'</td>
                     <td>'.$mobile.'</td>
                     <td>'.$password.'</td>
+                    <td>
+                    <button class="btn btn-primary"><a href="" class="text-light">Update</a></button>
+                    <button class="btn btn-danger"><a href="" class="text-light">Delete</a></button>
+                </td>
                 </tr>';
                     }
                 }
                 ?>
-                <!-- <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr> -->
+
             </tbody>
         </table>
     </div>
